285c3e55c6e3cbce0e9acabbc53a1aa47c99feab
manhtrangtn <manhtrangtn@gmail.com>
2020-06-15 20:10:33 +0700

    Update project

---
+++ T1809E.TEAM5.F101.ONLINE_SMS_SERVICES/Services/SmsService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T1809E.TEAM5.F101.ONLINE_SMS_SERVICES.Services
+{
+    public class SmsService
+    {
+    }
+}
---
+++ T1809E.TEAM5.F101.ONLINE_SMS_SERVICES/Utils/CommonConstants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T1809E.TEAM5.F101.ONLINE_SMS_SERVICES.Utils
+{
+    public class CommonConstants
+    {
+      public const string Account_Sid = "AC3b088464b904f6f3a525c03963c28fed";
+      public const string Auth_Token = "47d7a8a58fc75805aa383f6f92aede02";
+      public const string My_Number = "+12058097440";
+    }
+}
